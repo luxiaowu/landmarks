6f401ad571cebe29458bcfd8e7f1f9f048b5fb98
路小武 <wb-lxw799634@antgroup.com>
2023-08-15 16:05:13 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 路小武 on 2023/8/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
